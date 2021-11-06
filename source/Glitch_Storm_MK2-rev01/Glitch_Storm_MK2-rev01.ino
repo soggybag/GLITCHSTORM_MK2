3373a34c6cbe04324691176a2fa16f0d5186eff6
Mitchell Hudson <soggybag@gmail.com>
2021-11-06 14:33:47 -0700

    Update Glitch_Storm_MK2-rev01.ino

--- source/Glitch_Storm_MK2-rev01/Glitch_Storm_MK2-rev01.ino
+++ source/Glitch_Storm_MK2-rev01/Glitch_Storm_MK2-rev01.ino
@@ -10,18 +10,18 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 
-#define ledPin   13
-#define speakerPin 11
-#define leftButtonPin 2
+#define ledPin         13
+#define speakerPin     11
+#define leftButtonPin   2
 #define rightButtonPin A5
-#define progBit0Pin 7
-#define progBit1Pin 8
-#define progBit2Pin 12
-#define progBit3Pin 10
-#define clockOut0 6
-#define clockOut1 5
-#define clockOut2 4
-#define clockOut3 3
+#define progBit0Pin     7
+#define progBit1Pin     8
+#define progBit2Pin    12
+#define progBit3Pin    10
+#define clockOut0       6
+#define clockOut1       5
+#define clockOut2       4
+#define clockOut3       3
 
 #define isDebugging false
 
@@ -30,24 +30,30 @@
 //int SAMPLE_RATE = 9216;
 //int SAMPLE_RATE = 22000;
 
-int debounceRange = 20;// 5
+int debounceRange = 20; // 5
+
 long t = 0;
 volatile int a, b, c;
 volatile int value;
+
 byte programNumber = 1;
 byte upButtonState = 0;
 byte downButtonState = 0;
 byte lastButtonState = 0;
 byte totalPrograms = 16;
-byte clocksOut = 0;
+byte clocksOut = 0; 
+
 int cyclebyte = 0;
+
 volatile int aTop = 99;
 volatile int aBottom = 0;
 volatile int bTop = 99;
 volatile int bBottom = 0;
 volatile int cTop = 99;
 volatile int cBottom = 0;
+
 int d = 0;
+
 bool isClockOutMode = false;
 bool isSerialValues = true;
 unsigned long time_now = 0;
@@ -56,6 +62,7 @@ long button1Timer = 0;
 long longPress1Time = 400;
 long button2Timer = 0;
 long longPress2Time = 400;
+
 boolean isButton1Active = false;
 boolean isLongPress1Active = false;
 boolean isButton2Active = false;
@@ -65,8 +72,10 @@ int  shift_A_Pot = 1;
 int  old_A_Pot = 1;
 int SAMPLE_RATE = 16384;
 int old_SAMPLE_RATE = SAMPLE_RATE;
+
 byte shift_C_Pot = 0;
 byte old_C_Pot = 0;
+
 bool clockOut0bit = 0;
 
 
@@ -78,8 +87,7 @@ void setup() {
   pinMode(progBit3Pin, OUTPUT);
   pinMode(clockOut0, OUTPUT);
   pinMode(clockOut1, OUTPUT);
-  //  pinMode(clockOut2, OUTPUT);
-
+  // pinMode(clockOut2, OUTPUT);
 
   pinMode(leftButtonPin, INPUT_PULLUP);
   pinMode(rightButtonPin, INPUT_PULLUP);
@@ -93,11 +101,8 @@ void setup() {
 }
 
 void loop() {
-
-
   buttonsManager();
   potsManager();
-
 
   //slow loop show serial once every second
   if (isDebugging) {
@@ -118,7 +123,6 @@ void loop() {
   // this only if sample rate is different.
   // OCR1A = F_CPU / SAMPLE_RATE;
 
-
 }
 
 
@@ -128,24 +132,18 @@ void potsManager() {
     a = map(analogRead(0), 0, 1023, aBottom, aTop);
     b = map(analogRead(1), 0, 1023, bBottom, bTop);
     c = map(analogRead(2), 0, 1023, cBottom, cTop);
-
   }
 
   sampleRateManager();
 
   if (isLongPress2Active) {
-
     //left button is pressed
     leftLongPressActions();
-
-
   }
-  if (isLongPress1Active) {
 
+  if (isLongPress1Active) {
     //right button is pressed
     rightLongPressActions();
-
-
   }
 
 }
@@ -155,11 +153,10 @@ void rightLongPressActions() {
   int actual_A_Pot = map(analogRead(0), 0, 1023, -7, 7);
 
   if (old_A_Pot != actual_A_Pot) {
-
     shift_A_Pot = actual_A_Pot;
   }
-  old_A_Pot = actual_A_Pot;
 
+  old_A_Pot = actual_A_Pot;
 
   if (shift_A_Pot == 0) {
     //prevents the engine to stop
@@ -174,7 +171,6 @@ void sampleRateManager() {
   old_SAMPLE_RATE = SAMPLE_RATE;
   //int actual_SAMPLE_RATE = analogRead(1);
   SAMPLE_RATE = softDebounce(analogRead(3), SAMPLE_RATE);
-
 
   // actual_SAMPLE_RATE=map(analogRead(1), 0, 1023, 256, 16384);
   if (SAMPLE_RATE != old_SAMPLE_RATE) {
